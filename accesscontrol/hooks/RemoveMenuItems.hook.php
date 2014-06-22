bb06a87721880f1596192b09719cc29fb1717acd
Tom Elliott <tommygunsster@gmail.com>
2014-06-22 23:52:52 +0000

    Build starting plugin for access control.
    git-svn-id: https://svn.code.sf.net/p/freeghost/code/trunk@1903 71f96598-fa45-0410-b640-bcd6f8691b32

---
+++ accesscontrol/hooks/RemoveMenuItems.hook.php
@@ -0,0 +1,43 @@
+<?php
+class RemoveMenuItems extends Hook
+{
+	var $name = 'RemoveMenuItems';
+	var $description = 'Removes menu items and restricts the links from the page.';
+	var $author = 'Tom Elliott';
+	var $active = true;
+	private $linkToFilter;
+	public function __construct()
+	{
+		parent::__construct();
+		$this->linksToFilter = array('host','storage','images');
+	}
+	public function MenuData($arguments)
+	{
+		foreach($arguments['main'] AS $link => $title)
+		{
+			if (in_array($link,$this->linksToFilter))
+				unset($arguments['main'][$link]);
+		}
+	}
+	public function SubMenuData($arguments)
+	{
+		foreach($arguments['submenu'] AS $node => $link)
+		{
+			if (in_array($node,$this->linksToFilter))
+				unset($arguments['submenu'][$node]);
+		}
+	}
+	public function NotAllowed($arguments)
+	{
+		if (in_array($_REQUEST['node'],(array)$this->linksToFilter))
+		{
+			$this->FOGCore->setMessage('Not Allowed!');
+			$this->FOGCore->redirect('index.php');
+		}
+	}
+}
+$RemoveMenuItems = new RemoveMenuItems();
+// Register hooks
+$HookManager->register('MAIN_MENU_DATA', array($RemoveMenuItems, 'MenuData'));
+$HookManager->register('SUB_MENULINK_DATA', array($RemoveMenuItems, 'SubMenuData'));
+$HookManager->register('CONTENT_DISPLAY', array($RemoveMenuItems, 'NotAllowed'));
